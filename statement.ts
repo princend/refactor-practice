b56da709f6c7b4cefe6c34756c3de0c9f6e6eb42
princend <waxdji3g4y3c04@gmail.com>
2020-05-14 13:24:14 +0800

    credits

--- statement.ts
+++ statement.ts
@@ -1,7 +1,23 @@
 import { Invoice, Play, Performance } from "./models";
 
 
-type Plays = { [key: string]: Play };
+
+type hash<T> = { [key: string]: T };
+type Plays = hash<Play>;
+type CbFn<T, P> = (value: T) => P;
+type DramaType = hash<CbFn<number, number>>;
+
+const dramaDic: DramaType = {
+    tragedy: (audience: number) => audience > 30 ? (40000 + 1000 * (audience - 30)) : 40000,
+    comedy: (audience: number) => audience > 20 ? 40000 + 500 * (audience - 20) + 300 * audience : 30000 + 300 * audience
+}
+
+const resultDic = {
+    init: (customer: string) => `Statement for ${customer}\n`,
+    order: (name: string, thisAmount: number, audience: number) => ` ${name}: ${formatUSD(thisAmount)} (${audience} seats)\n`,
+    end: (totalAmount: number, volumeCredits: number) => `Amount owed is ${formatUSD(totalAmount)}\nYou earned ${volumeCredits} credits!\n`
+}
+
 
 export default function statement(invoice: Invoice, plays: Plays): string {
     let totalAmount = 0;
@@ -10,16 +26,8 @@ export default function statement(invoice: Invoice, plays: Plays): string {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        thisAmount = calcAmount(play.type, perf.audience);
-
-        //add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        //add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        //print line for this order
+        let thisAmount = calcAmount(play.type, perf.audience);
+        volumeCredits += getVolumnCredits(perf.audience, play.type);
         result += resultDic['order'](play.name, thisAmount, perf.audience);
         totalAmount += thisAmount;
     }
@@ -35,6 +43,10 @@ const currencyUSD = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: 2
 })
 
+function getVolumnCredits(audience: number, type: string) {
+    return "comedy" === type ? Math.floor(audience / 5) + Math.max(audience - 30, 0) : Math.max(audience - 30, 0);;
+}
+
 function formatUSD(value: number): string {
     return currencyUSD.format(value / 100);
 }
@@ -49,16 +61,5 @@ function calcAmount(type: string, audience: number): number {
 }
 
 
-type dramaType = { [key: string]: (value: number) => number };
 
-const dramaDic: dramaType = {
-    tragedy: (audience: number) => audience > 30 ? (40000 + 1000 * (audience - 30)) : 40000,
-    comedy: (audience: number) => audience > 20 ? 40000 + 500 * (audience - 20) + 300 * audience : 30000 + 300 * audience
-}
-
-const resultDic = {
-    init: (customer: string) => `Statement for ${customer}\n`,
-    order: (name: string, thisAmount: number, audience: number) => ` ${name}: ${formatUSD(thisAmount)} (${audience} seats)\n`,
-    end: (totalAmount: number, volumeCredits: number) => `Amount owed is ${formatUSD(totalAmount)}\nYou earned ${volumeCredits} credits!\n`
-}
 
